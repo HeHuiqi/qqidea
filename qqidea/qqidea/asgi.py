cf85a65a4fa209ca626658a644df313ffb238b17
HeHuiqi <1710308677@qq.com>
2022-12-26 15:09:06 +0800

    create models

--- qqidea/qqidea/asgi.py
+++ qqidea/qqidea/asgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qqidea.settings')
+
 #  读取 QQIDEA_PROFILE 环境变量，如果没有取默认值develop
 profile = os.environ.get('QQIDEA_PROFILE','develop')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qqidea.settings.%s' % profile)
